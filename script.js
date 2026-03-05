4fdac37e59a2672c96904cca38cc10b8a0c334af
angel <211508797+angela-spirova@users.noreply.github.com>
2026-03-05 13:59:09 +0100

    Modify div where results are displayed

--- script.js
+++ script.js
@@ -22,6 +22,10 @@ buttons.forEach((button) => {
     });
 });
 
+function displayCurrentRoundResult(result){
+    currentRoundResult.textContent = result;
+}
+
 function playRound(humanChoice, computerChoice){
     if(humanChoice===computerChoice){
         console.log(`It's a tie! (both sides chose ${humanChoice})`);
